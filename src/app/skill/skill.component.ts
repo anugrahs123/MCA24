e089153121504476805e427922269e55e5e6a633
anugrahs123 <anugrahsasikumar@gmail.com>
2022-11-26 21:52:53 +0530

    team

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import Team from './../Team.json'
 @Component({
   selector: 'app-skill',
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.css']
 })
 export class SkillComponent implements OnInit {
-
+  public TeamList : any ;
   constructor() { }
 
   ngOnInit(): void {
+    this.TeamList=Team;
+    console.log(this.TeamList);
+
+    
   }
 
 }
