52f7bc73bcd6d34ab2067fdb3b7e6166c143b7bc
anugrahs123 <anugrahsasikumar@gmail.com>
2022-11-29 23:35:36 +0530

    pic_second

---
+++ docs/assets/js/slider.d.ts
@@ -0,0 +1,1 @@
+export declare function active_course():any
--- src/app/fact/fact.component.ts
+++ src/app/fact/fact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {active_course} from './../../assets/js/slider'
 
 @Component({
   selector: 'app-fact',
@@ -8,8 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class FactComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit(): void {
+    active_course()
   }
 
 }
